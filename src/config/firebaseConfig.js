c5c431299472a84d073408bfd00b9441364fbee1
Rahman Nugar <cladeadenugar@gmail.com>
2022-06-22 13:35:37 +0100

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBMfHUVXe4GDHryY7sJMPmnElS5kLDZZo4",
-  authDomain: "crypto-hunter-50991.firebaseapp.com",
-  projectId: "crypto-hunter-50991",
-  storageBucket: "crypto-hunter-50991.appspot.com",
-  messagingSenderId: "141181984273",
-  appId: "1:141181984273:web:ba51a25e27ce6a2b1d9bb0",
+  apiKey: "AIzaSyDMGZNuft6vk8qIHY-TKkjqqU8K-SdjR0M",
+  authDomain: "cryptracker-61f52.firebaseapp.com",
+  projectId: "cryptracker-61f52",
+  storageBucket: "cryptracker-61f52.appspot.com",
+  messagingSenderId: "135693067730",
+  appId: "1:135693067730:web:5e37c6078f6d3b38d4940b"
 };
-
 export default firebaseConfig;
